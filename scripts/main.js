2037540a1f5210f1ecd5c719905423522a931423
Rohit42 <rohit.mittapalli@gmail.com>
2018-07-29 16:42:36 -0500

    Animated Timeline

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,10 @@ function resizedesk() {
     $('.stretchy-wrapper').css('padding-bottom',  9 * max_width / 16);
 } 
 
+$(function(){  // $(document).ready shorthand
+  $('.monster').fadeIn('slow');
+});
+
 window.onresize = resizedesk
 
 $(document).ready(function(){
@@ -23,4 +27,23 @@ $(document).ready(function(){
         }
 
     });
-});
+    
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.container_timeline').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1000);
+                    
+            }
+            
+        }); 
+    
+    });
+    });
