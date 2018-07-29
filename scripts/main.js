dbc80200f88d2c7ab263cfde54c27c4f3767f956
Rohit42 <rohit.mittapalli@gmail.com>
2018-07-29 15:36:48 -0500

    Dynamic Resizing of Desk

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
+function resizedesk() {
+    var window_height = $(window).height();
+    var window_width = $(window).width();
+    window_height = 16 * window_height / 9
+    var max_width = Math.min( window_height, window_width);
+
+    $('.stretchy-wrapper').css('width', max_width);
+    $('.stretchy-wrapper').css('padding-bottom',  9 * max_width / 16);
+} 
+
+window.onresize = resizedesk
+
 $(document).ready(function(){
     $('a[href^="#"]').on('click', function(event) {
 
